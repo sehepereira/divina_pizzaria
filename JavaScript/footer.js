a41972fa5f58bddefb52ea046eb24c5e927e4665
Sergio Pereira <sehepereira@gmail.com>
2023-12-02 15:45:22 -0300

    adicionado novamente remocoes de cabecalho e rodape unico

--- JavaScript/footer.js
+++ JavaScript/footer.js
@@ -2,25 +2,39 @@ const footer = document.createElement('footer');
 
 footer.innerHTML = `
     <footer>
-        <section class="container__funcionamento">
-            <h4 class="cabecalho__funcionamento__texto">Funcionamento</h4> 
-            <div class="menu__container__funcionamento">
-                <table class="container__tabela">
-                    <thead class="cabecalho__tabela">
-                        <tr class="cabecalho__dias">
-                            <th>Dia</th>
-                            <th>Horário</th>
-                        </tr>
-                    </thead>
-                </table> 
-            </div>
-            <div class="rodape__localizacao">
-                <p>Divino Pizzaria</p>
-                <img class="logo__frame" src="../IMG/Frame.png" alt="Logo localização">
-                <p><a class="link__rodape" target="_blank" href="https://maps.app.goo.gl/pnuHPz6e1sw1Yps58">AV.Secreta tenta de novo,1010</a></p>
-            </div>
-            <p class="talento__cloud">Talento Cloud 2023</p>
-        </section>
+      <section class="container__footer">
+        <ul class="container__lista__horario__dia">
+          <li class="cabecalho__texto__horario">Horários</li><br>
+          <li class="texto__dias__funcionamento">Terça a Quinta Feira</li><br>
+          <li class="texto__horario__funcionamento">18:30 a 00:00</li><hr>
+          <li class="texto__dias__funcionamento">Sexta a Domingo</li><br>
+          <li class="texto__horario__funcionamento">18:00 a 01:20</li><hr>
+        </ul>
+       
+        <ul class="container__rede__social">
+          <li class="texto__contato">Contato</li><br>
+          <li class="texto__rede__sociais">Fone (xx) xxxxx-xxxx</li><br>
+          <li class="texto__rede__sociais">whats (xx) xxxxx-xxxx</li><br>
+        </ul> 
+        <ul class="lista__redes__sociais">
+          <li class="rede__social__texto">Siga-nos em nossas redes sociais</li><br>
+          <img class="rede__social" src="/IMG/icon-insta.png" alt="">
+          <img class="rede__social" src="/IMG/icon-face.png" alt="">
+          <img class="rede__social" src="/IMG/icone-zap.png" alt="">
+        </ul><br>
+        
+        <ul class="rodape__localizacao">
+          <li class="texo__divino__pizzaria">Divino Pizzaria</li>
+          <li><img class="logo__frame" src="../IMG/Frame.png" alt="Logo localização"></li>
+          <li>
+              <a
+                class="link__rodape"
+                target="_blank"
+                href="https://maps.app.goo.gl/pnuHPz6e1sw1Yps58">AV.Secreta tenta de novo, 1010.
+              </a>
+          </li><hr>
+        </ul>
+      </section>
     </footer>
 `;
 
--- JavaScript/header.js
+++ JavaScript/header.js
@@ -1,40 +1,50 @@
 const header = document.createElement('header');
 
 header.innerHTML = `
-<header class="cabecalho">
-<img class="cabecalho__logo__pizzadivina" src="../IMG/logo 1.png" alt="Logo divina-pizzaria">
-<div class="container">
-    <nav class="container__lista__navegacao">
-        <input type="checkbox" id="menu" class="container__botao">
-        <label for="menu">
+    <header class="cabecalho">
+      <img
+      class="cabecalho__logo__pizzadivina"
+      src="../IMG/logo 1.png"
+      alt="Logo divina-pizzaria"/>
+     
+      <div class="container">
+        <nav class="container__lista__navegacao">
+          <input type="checkbox" id="menu" class="container__botao" />
+          <label for="menu">
             <span class="cabecalho__menu__hamburguer"></span>
-        </label>
-        <ul class="lista__menu">
-            <li class="lista__menu__informacoes"></li>
+          </label>
+          <ul class="lista__menu">
             <li class="lista__menu__item">
-                <a href="../HTML/index.html" class="lista__menu__link">Inicio</a>
+              <a href="../HTML/index.html" class="lista__menu__link">Inicio</a>
             </li>
             <li class="lista__menu__item">
-                <a href="../HTML/cardapio.html" class="lista__menu__link">Cardápio</a>
+              <a href="../HTML/cardapio.html" class="lista__menu__link"
+                >Cardápio</a>
             </li>
-        </ul>
-    </nav>
-</div>
-<div class="container__carrinho">
-    <nav class="container__carrinho__navegacao">
-        <ul class="login">
-            <img class="imagem__login" src="../IMG/img-login.png" alt="Logo-login">
+          </ul>
+        </nav>
+      </div>
+      <div class="container__carrinho">
+        <nav class="container__carrinho__navegacao">
+          <ul class="login">
+            <img
+              class="imagem__login"
+              src="../IMG/img-login.png"
+              alt="Logo-login"/>
             <li class="lista__menu__login">
-                <a class="lista__menu__login" href="../HTML/login.html">Login</a>
+              <a class="lista__menu__login" href="../HTML/login.html">Login</a>
             </li>
-            <img class="imagem__carrinho" src="../IMG/carrinho.png" alt="Imagem carrinho">
+            <img
+              class="imagem__carrinho"
+              src="../IMG/carrinho.png"
+              alt="Imagem carrinho"/>
             <li class="lista__menu__carrinho">
-                <a class="lista__menu__carrinho" href="../HTML/carrinho.html">Carrinho</a>
+              <a class="lista__menu__carrinho" href="../HTML/carrinho.html">Carrinho</a>
             </li>
-        </ul>
-    </nav>
-</div>
-</header>
+          </ul>
+        </nav>
+      </div>
+    </header>
 `;
 
 document.body.prepend(header);
