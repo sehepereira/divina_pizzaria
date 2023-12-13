fa3054720c0194067ead4712b6217e32abe2de06
Sergio Pereira <sehepereira@gmail.com>
2023-12-13 18:57:44 -0300

    adicionando itens no carrinho

--- JavaScript/cardapio.js
+++ JavaScript/cardapio.js
@@ -1,27 +1,40 @@
-var botaoAdicionaProduto = document.querySelector("#botaoAdicionaProduto");
-
-botaoAdicionaProduto.addEventListener("click", function(event) {
-    event.preventDefault();
-
-    const button = event.target
-	const productInfos = button.parentElement.parentElement
-	const productImage = productInfos.getElementsByClassName("logo__imagem__cardapio")[0].src
-	const productName = productInfos.getElementsByClassName("titulo-produto")[0].innerText
-	const productValue = productInfos.getElementsByClassName("preco-produto")[0].innerText
-    
-    // Define o item que será inserido no carrinho
-    const novoItem = { imgem: productImage, nome: productName, valor: productValue}; // Por padrão, a quantidade é 1
-    
-    // Recupera os itens existentes no carrinho (se houver)
-    const itensCarrinho = JSON.parse(localStorage.getItem('itensCarrinho')) || [];
-    
-    // Adiciona o novo item à lista de itens no carrinho
-    itensCarrinho.push(novoItem);
-    
-    // Atualiza o localStorage com a nova lista de itens do carrinho
-    localStorage.setItem('itensCarrinho', JSON.stringify(itensCarrinho));
-    
-    // Redireciona para a página do carrinho
-    window.location.href = 'carrinho.html';
-
+// Evento de clique delegado para os botões
+document.addEventListener('click', function(event) {
+  if (event.target.classList.contains('botao-adiciona-produto')) {
+    adicionarItemAoCarrinho(event);
+  }
 });
+
+function adicionarItemAoCarrinho(event){
+
+  const button = event.target;
+  const productInfos = button.parentElement.parentElement;
+  const productImage = productInfos.querySelector(
+    ".logo__imagem__cardapio"
+  ).src;
+  
+  const productName =
+    productInfos.getElementsByClassName("titulo-produto")[0].innerText;
+  const productValue =
+    productInfos.getElementsByClassName("preco-produto")[0].innerText;
+
+  // Define o item que será inserido no carrinho
+  const novoItem = {
+    imagem: productImage,
+    nome: productName,
+    valor: productValue,
+    quantidade: 1,
+  }; // Por padrão, a quantidade é 1
+
+  // Recupera os itens existentes no carrinho (se houver)
+  const itensCarrinho = JSON.parse(localStorage.getItem("itensCarrinho")) || [];
+
+  // Adiciona o novo item à lista de itens no carrinho
+  itensCarrinho.push(novoItem);
+
+  // Atualiza o localStorage com a nova lista de itens do carrinho
+  localStorage.setItem("itensCarrinho", JSON.stringify(itensCarrinho));
+
+  // Redireciona para a página do carrinho
+  window.location.href = "carrinho.html";
+};
